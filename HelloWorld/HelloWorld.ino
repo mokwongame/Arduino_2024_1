b519f2796fe8230d2566335cc72a64ed341692d7
mokwongame <ictplusud@gmail.com>
2024-03-28 11:54:46 +0900

    HelloWorld 프로젝트 추가

---
+++ HelloWorld/HelloWorld.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Serial: 시리얼 통신 클래스의 인스턴스(정의된 변수)
+  Serial.begin(9600); //. 의미: 클래스의 멤버 접근 방법; begin(): 시리얼 통신 시작 함수
+  // 9600 의미: 시리얼 통신의 속도(bps: bit(baud) per second); Serial Monitor의 속도와 동일해야 함
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
