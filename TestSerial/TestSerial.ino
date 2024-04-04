f6291c1ea06f2e9015dff7c5c1620fef37ef3fe3
mokwongame <ictplusud@gmail.com>
2024-04-04 12:15:15 +0900

    Serial 입력을 숫자로 바꾸는 기능 추가

--- TestSerial/TestSerial.ino
+++ TestSerial/TestSerial.ino
@@ -9,6 +9,11 @@ void loop() {
   // Serial에서 입력이 있다면 { 코드 블록 } 실행
   if (Serial.available()) {   // Serial 버퍼가 사용 가능한지? 외부에서 입력이 있어서 버퍼를 채웠는가?; 버퍼가 채워졌으면 true를 출력, 아니면 false
     char ch = Serial.read();  // Serial에서 버퍼에 채워진 입력을 한 바이트 읽기
+    Serial.print("문자 = ");
     Serial.println(ch);
+    // 문자를 숫자로 바꾸기: 예) 문자 '3' -> 숫자 3(이진수로 11)
+    int i = String(ch).toInt();  // char ch를 String 자료형으로 type casting
+    Serial.print("숫자 = ");
+    Serial.println(i);
   }
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "TestSerial 프로젝트 추가"
+git commit -m "Serial 입력을 숫자로 바꾸는 기능 추가"
 git push -u origin master
