e17d912ca6a1a9771922276e9c9fb742718b2a19
mokwongame <ictplusud@gmail.com>
2024-04-11 11:22:01 +0900

    blinkLed() 함수 완성

--- InputBlink/InputBlink.ino
+++ InputBlink/InputBlink.ino
@@ -1,4 +1,7 @@
 #define SERIAL_BPS (9600)
+#define PIN_LED (13)
+#define LED_ON_TIME (200) // in msec
+#define LED_OFF_TIME (300) // in msec
 
 // int userInput() {
 //   if (Serial.available()) {
@@ -17,9 +20,18 @@ int userInput() {
   return i;
 }
 
+void blinkLed() {
+  digitalWrite(PIN_LED, HIGH);  // 13번 디지털 포트에 HIGH 전압(5V) 출력
+  delay(LED_ON_TIME);
+  digitalWrite(PIN_LED, LOW);  // 13번 디지털 포트에 LOW 전압(0V, GND) 출력
+  delay(LED_OFF_TIME);
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(SERIAL_BPS);
+  pinMode(PIN_LED, OUTPUT);  // 13번 디지털 포트를 출력으로 설정
+  digitalWrite(PIN_LED, LOW); // LED off로 초기화
 }
 
 void loop() {
