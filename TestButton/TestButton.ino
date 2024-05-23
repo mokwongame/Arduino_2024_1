8e98efbb8d93d36e2bb02040c53eb1d34a2ee539
mokwongame <ictplusud@gmail.com>
2024-05-23 11:50:43 +0900

    에지 검출 추가

--- TestButton/TestButton.ino
+++ TestButton/TestButton.ino
@@ -1,16 +1,30 @@
+int nInputPrev;  // 예전 입력
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   //pinMode(6, INPUT); // 위험해서 잘 사용하지 않음: 접지(GND)는 문제 없으나 5V는 위험함
   pinMode(6, INPUT_PULLUP);
+  nInputPrev = digitalRead(6);  // 현재 상태 저장
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int nInput = digitalRead(6); // 평상시는 5V(HIGH); 버튼 누르면 0V(LOW); 푸시버튼에 접지를 연결한 상태
+  int nInput = digitalRead(6);  // 평상시는 5V(HIGH); 버튼 누르면 0V(LOW); 푸시버튼에 접지를 연결한 상태
   // 스위치의 누른 상태를 확인(전압으로 확인)
-  String sInput = (nInput == HIGH) ? "open" : "closed";
-  // 버튼을 누르면 회로가 닫힘(closed); 떼면 회로가 열림(open)
-  Serial.println(sInput);
-  delay(1000);
+  // String sInput = (nInput == HIGH) ? "open" : "closed";
+  // // 버튼을 누르면 회로가 닫힘(closed); 떼면 회로가 열림(open)
+  // Serial.println(sInput);
+  // delay(1000);
+
+  // 에지 검출: 빠른 응답 가능(마우스 클릭에도 사용)
+  if (nInput != nInputPrev) {  // 에지가 있음
+    bool bRise;
+    if (nInput == HIGH)  // LOW -> HIGH: rise
+      bRise = true;
+    else bRise = false; // HIGH -> LOW: fall
+    String sInput = (bRise) ? "upward" : "downward";
+    Serial.println(sInput);
+    nInputPrev = nInput; // 현재 상태를 다시 저장
+  }
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "TestButton 프로젝트 추가"
+git commit -m "에지 검출 추가"
 git push -u origin master
